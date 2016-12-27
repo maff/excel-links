d4915cafc20238dcc8d5f9c2ae4fad433af1038a
Mathias Geat <mathias@ailoo.net>
2016-12-27 21:26:44 +0100

    WIP: upload file and move it to var/files

--- config/prod.php
+++ config/prod.php
@@ -3,5 +3,6 @@
 // configure your app for the production environment
 
 $app['version']      = '0.0.1';
+$app['file_dir']     = __DIR__ . '/../var/files';
 $app['twig.path']    = array(__DIR__ . '/../templates');
 $app['twig.options'] = array('cache' => __DIR__ . '/../var/cache/twig');
--- src/controllers.php
+++ src/controllers.php
@@ -1,14 +1,45 @@
 <?php
 
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
+/** @var \Silex\Application $app */
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array());
 })
 ->bind('homepage')
+;
+
+$app->post('/process', function (Request $request) use ($app) {
+    $fileNames = $request->get('fileNames');
+    if (!$fileNames) {
+        return $app->json([
+            'error' => 'No filenames found',
+        ], 400);
+    } else {
+        $fileNames = explode(',', $fileNames);
+    }
+
+    /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $excelFile */
+    $excelFile = $request->files->get('excelFile');
+    if (!$excelFile) {
+        return $app->json([
+            'error' => 'No excel file found',
+        ], 400);
+    }
+
+    $uuid = Uuid::uuid4();
+    $file = $excelFile->move($app['file_dir'], $uuid);
+
+    return $app->json([
+        'excelFile' => $file->getRealPath(),
+        'fileNames' => $fileNames,
+    ]);
+})
+->bind('process')
 ;
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
