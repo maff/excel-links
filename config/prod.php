1a3d4ae5699f17029d0b9b1e0927eee764197440
Mathias Geat <mathias@ailoo.net>
2016-12-27 13:11:50 +0100

    Reformat silex skeleton

--- config/prod.php
+++ config/prod.php
@@ -2,5 +2,6 @@
 
 // configure your app for the production environment
 
-$app['twig.path'] = array(__DIR__.'/../templates');
-$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['version']      = '0.0.1';
+$app['twig.path']    = array(__DIR__ . '/../templates');
+$app['twig.options'] = array('cache' => __DIR__ . '/../var/cache/twig');
