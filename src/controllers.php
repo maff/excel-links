4e8b335cbd4cdc618b1d1f88020f2bb04e6f7e1f
Mathias Geat <mathias@ailoo.net>
2016-12-30 20:02:36 +0100

    Excel upload and modify working more or less (rendering broken excel file)

--- src/controllers.php
+++ src/controllers.php
@@ -1,8 +1,10 @@
 <?php
 
 use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
@@ -34,12 +36,61 @@ $app->post('/process', function (Request $request) use ($app) {
     $uuid = Uuid::uuid4();
     $file = $excelFile->move($app['file_dir'], $uuid);
 
+    $excel = \PHPExcel_IOFactory::load($file->getRealPath());
+    $sheet = $excel->createSheet();
+
+    // file:///L:/Erlerstrasse 1/__Abgabe/Reduziert/Erle_1_0367.jpg
+    $fullPath = 'L:\\Erlerstrasse';
+    $fullPath = str_replace('\\', '/', $fullPath);
+    $fullPath = trim($fullPath);
+    $fullPath = trim($fullPath, '/');
+
+    for ($i = 0; $i < count($fileNames); $i++) {
+        $filePath = $fullPath . '/' . $fileNames[$i];
+        $fileUri  = 'file:///' . $filePath;
+
+        $sheet->setCellValue('A' . $i, $fileNames[$i]);
+
+        $linkCell = 'B' . $i;
+
+        $sheet->setCellValue($linkCell, $filePath);
+        $sheet->getCell($linkCell)->getHyperlink()->setUrl($fileUri);
+    }
+
+    $saveName = $uuid . '_edited.xlsx';
+    $savePath = $app['file_dir'] . '/' . $saveName;
+
+    $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+    $writer->save($savePath);
+
     return $app->json([
-        'excelFile' => $file->getRealPath(),
-        'fileNames' => $fileNames,
+        'excelFile'   => $file->getRealPath(),
+        'savePath'    => $savePath,
+        'fileNames'   => $fileNames,
+        'downloadUri' => '/download/' . $uuid
     ]);
 })
 ->bind('process')
+;
+
+$app->get('/download/{uuid}.xlsx', function($uuid) use ($app) {
+    $filename = $uuid . '_edited.xlsx';
+    $file = $app['file_dir'] . '/' . $filename;
+
+    if (!file_exists($file)) {
+        $app->abort(404, 'File does not exist');
+    }
+
+    $response = new BinaryFileResponse(new SplFileInfo($file));
+    $response->setContentDisposition(
+        ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+        $filename,
+        iconv('UTF-8', 'ASCII//TRANSLIT', $filename)
+    );
+
+    return $response;
+})
+->bind('download')
 ;
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
